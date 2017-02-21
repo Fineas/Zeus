e34d253df12d22552b84ddcebb41ffd6649562ca
Fineas <finymelly@gmail.com>
2017-02-21 17:00:55 +0200

    Dictionary errors fixed

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,4 @@
 import numpy as np
-import urllib2
-import requests
-
-response = urllib2.urlopen("http://google.ro")
-page_source = response.read()
-
-data = urllib2.urlopen("http://www.google.com").read(20000) # read only 20 000 chars
-data = data.split("\n") # then split it into lines
-
-for line in data:
-    print line
 
 
 #
